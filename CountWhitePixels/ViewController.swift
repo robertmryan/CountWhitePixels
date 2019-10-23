1689b897be1514b1dd02d34a3c8d2144603a3f87
Robert Ryan <robert.ryan@mindspring.com>
2019-10-22 18:13:53 -0700

    Initial Commit

---
+++ CountWhitePixels/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CountWhitePixels
+//
+//  Created by Robert Ryan on 10/22/19.
+//  Copyright © 2019 Robert Ryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
